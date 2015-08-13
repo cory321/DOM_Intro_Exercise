9614d20fb6a11e7445eb662f35f41c1b8b7bd128
Cory Williams <cory321@gmail.com>
2015-08-13 15:58:42 -0700

    init commit

---
+++ script.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+	//wait until everything has been loaded
+
+	console.log("JavaScript is alive");
+    document.getElementById("greeting").innerText = "Hello Planet Earth";
+    
+    addClickListener();
+    resetButton();
+};
+
+var li = document.getElementsByTagName("li");
+
+addClickListener = function()  {
+
+	for (var i = 0; i < li.length; i++) {
+
+		li[i].addEventListener("click", function(){ 
+
+			console.log(this); 
+			this.classList.add("selected");
+			this.style.backgroundColor = "Yellow";
+
+		});
+	}
+};
+
+resetButton = function() {
+	var resetButton = document.getElementById("reset");
+	resetButton.addEventListener("click", function() { 
+		
+		for (var i = 0; i < li.length; i++) {
+			li[i].style.backgroundColor = "White";
+			li[i].classList.remove("selected");
+		}
+	});
+};
